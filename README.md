42c5920ee9143ee93abd6e8b9f4831634c0ea75a
Mdhvince <vinceslas.medhy@gmail.com>
2021-01-20 13:23:04 +0100

    More visual

---
+++ README.md
@@ -0,0 +1,5 @@
+## SudoAir
+Goal: Desining controller(s) in C++ for the CrazyFlie mini drone and additional computer vision based modules to allow the drone to navigate in total autonomy in complex environment.  
+
+#### Current PID response on z-axis
+![alt text](docs/PID-Tune.png)  
--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <memory>
 #include <array>
-#include <algorithm>
-#include <unistd.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include "controllib/include/controllib/pid_controller.h"
 
 using std::unique_ptr;
 using std::make_unique;
 
+void draw_pid_response(cv::Mat img, int step, float z){
+    cv::Point p = cv::Point(step*step, z);
+    circle(img, p, 2, cv::Scalar(0, 255, 0), 1, cv::LINE_8);
+}
 
 std::array<float, 2> update_state_z(float dt, float acceleration_z, float velocity_z, float position_z){
 
@@ -38,37 +44,38 @@ std::array<float, 2> update_state_z(float dt, float acceleration_z, float veloci
 }
 
 int main(){
-    unsigned int microsecond = 1000000;
     float max_motor_thrust_N = 0.58;
     float drone_mass_KG = 0.027;
     unique_ptr<PID_Controller> pid = make_unique<PID_Controller>(max_motor_thrust_N, drone_mass_KG);
     
     std::shared_ptr<float> z { new float(0.0) };
-    float z_des=10.0;
-    float vel_z_des=0.10;
+    float z_des=250.0;
+    float vel_z_des=0.20;
     std::shared_ptr<float> vel_z { new float(0.0) };
-    float dt=0.5;
-
+    float dt=1.0;
 
     std::shared_ptr<float> z_ptr {z};
     std::shared_ptr<float> vel_z_ptr {vel_z};
 
+    
+    cv::Mat img = cv::Mat::zeros(500, 1000, CV_8UC3);
+    cv::line(img, cv::Point(0, z_des), cv::Point(1000, z_des), cv::Scalar(0, 0, 255), 1, cv::LINE_8);
+
     for(int step{0}; step <= 100; step++){
         float thrust = pid->altitude_control(*z, z_des, vel_z_des, *vel_z, dt);
         float acc = pid->get_acceleration_z();
-
-        std::cout<< step <<std::endl;
-        std::cout<<"Pos: " << *z <<std::endl;
-        std::cout<<"New Velocity: " << acc * dt <<std::endl;
-        std::cout<<"New Thrust(N): " << thrust <<std::endl;
-        std::cout<<"Thrust/Weight ratio: " << (thrust*0.101)/drone_mass_KG <<std::endl;
         
         std::array<float, 2> s = update_state_z(dt, acc, *vel_z, *z);
         *z_ptr = s.at(1);
         *vel_z_ptr = s.at(0);
-        // usleep(0.1 * microsecond);
-        std::cout<< "\n";
+
+        draw_pid_response(img, step, *z);
     }
+
+    cv::namedWindow("Display", cv::WINDOW_NORMAL);
+    cv::flip(img, img, 0);
+    cv::imshow("Display", img);
+    int k = cv::waitKey(0);
     
     std::cout<< "\n\n";
     return 0;
